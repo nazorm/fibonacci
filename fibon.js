9230ca14828a89dc3aa3b30d34dade00e2737cab
Nazorm <nnadoziechinazor@gmail.com>
2020-04-28 21:18:46 +0100

    add fibonacci code

---
+++ fibon.js
@@ -0,0 +1,29 @@
+
+
+
+function printSequence(number){
+    let a = 0;
+    let b = 1;
+    let newSequence = [];
+
+for(i=0; i<number; i++){
+    if(i === 0){
+        newSequence.push(0) 
+        continue;
+    }
+    if (i === 1){
+        newSequence.push(1)
+        continue;
+    }
+    
+  let c = a+b;
+    a = b;
+    b = c;
+    newSequence.push(c);
+}
+return newSequence;
+}
+printSequence(8);
+
+
+   
